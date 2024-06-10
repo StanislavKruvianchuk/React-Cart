c8e0e62e37b7960d1ebca1ddce5a019a7068c7f6
Stanislav Kruvianchuk <kruvianchuk@gmail.com>
2024-06-10 14:55:53 +0300

    first commit

---
+++ src/components/ButtonDelete/ButtonDelete.jsx
@@ -0,0 +1,10 @@
+
+const ButtonDelete = () => {
+    return ( 
+        <button type="button">
+            <img src="./img/icons/cross.svg" alt="Delete" />
+        </button>
+     );
+}
+ 
+export default ButtonDelete;
---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,19 @@
+import CartFooter from '../CartFooter/CartFooter';
+import CartHeader from '../CartHeader/CartHeader'
+import Product from '../Product/Product'
+
+const Cart = () => {
+    return ( 
+        <section className="cart">
+
+            <CartHeader />
+
+            <Product />
+            <CartFooter />
+
+            
+        </section>
+     );
+}
+ 
+export default Cart;
